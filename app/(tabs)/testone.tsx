e77fcf0279b62fa4e6777ae99312be524d154907
ronaldolimacloud <ronaldolimacloud@gmail.com>
2025-08-23 22:38:41 +1000

    Initial commit: Setup Amplify Google Sign-in project with cleaned up structure

---
+++ app/(tabs)/testone.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function TestOne() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Test One</Text>
+    </View>
+  );
+}
